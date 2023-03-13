d11d674e769a35c4295ad7404bfbd6412f832299
zainuddineffendi <zainuddin.efendi@cadit.com.sg>
2023-03-13 14:04:24 +0700

    update

--- README.md
+++ README.md
@@ -639,6 +639,6 @@ Understand how to read interfaces in the standard lib. Writing your own interfac
 
 ## Channels and Go routine
 
-
+![Chat Preview](https://github.com/zainuddin-maker/Golang/blob/master/imgdiagram/diagrams-001%20-%20status.png?raw=true)
 
 ![Chat Preview](https://github.com/zainuddin-maker/Golang/blob/master/imgdiagram/diagrams-015%20-%20slice%20selection.png?raw=true)
---
+++ channels/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string{
+		"http://agosogle.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	for _, link := range links {
+
+		checkLink(link)
+	}
+}
+
+func checkLink(link string) {
+	_, err := http.Get(link)
+
+	if err != nil {
+		fmt.Println(link,"might be down")
+		return
+	}
+
+	fmt.Println(link,"is up!")
+}
